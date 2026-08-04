cbb8eec6cf35b96ff0519d26e0539a531fb5d27e
spaynetaxes-debug <spaynetaxes@gmail.com>
2026-08-04 04:37:08 -0400

    feat(movie-buff): export Buster and transition visuals

--- src/components/movie-buff/visual/index.ts
+++ src/components/movie-buff/visual/index.ts
@@ -1,6 +1,14 @@
+export {
+  MovieBuffBusterReplacement,
+  type MovieBuffBusterVisualState,
+} from "./MovieBuffBusterReplacement";
 export { MovieBuffGameMenu } from "./MovieBuffGameMenu";
 export { MovieBuffMotionRuntime } from "./MovieBuffMotionRuntime";
 export { MovieBuffReconnectVisual } from "./MovieBuffReconnectVisual";
 export { MovieBuffRiveSurface } from "./MovieBuffRiveSurface";
 export { MovieBuffStaticFallback } from "./MovieBuffStaticFallback";
+export {
+  MovieBuffTransitionSurface,
+  type MovieBuffTransitionKind,
+} from "./MovieBuffTransitionSurface";
 export { MovieBuffUsedTileStamp } from "./MovieBuffUsedTileStamp";
