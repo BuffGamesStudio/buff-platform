d9139c7a7f6628efdc032326db4b099999b2e8c3
spaynetaxes-debug <spaynetaxes@gmail.com>
2026-08-04 04:22:03 -0400

    feat(movie-buff): export visual runtime components

---
+++ src/components/movie-buff/visual/index.ts
@@ -0,0 +1,6 @@
+export { MovieBuffGameMenu } from "./MovieBuffGameMenu";
+export { MovieBuffMotionRuntime } from "./MovieBuffMotionRuntime";
+export { MovieBuffReconnectVisual } from "./MovieBuffReconnectVisual";
+export { MovieBuffRiveSurface } from "./MovieBuffRiveSurface";
+export { MovieBuffStaticFallback } from "./MovieBuffStaticFallback";
+export { MovieBuffUsedTileStamp } from "./MovieBuffUsedTileStamp";
